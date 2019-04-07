e3786911b1214f14476049cf58b2e4e7ee526e92
GibiDroidZ <gibs.teodosio@gmail.com>
2019-04-07 12:29:32 +0800

    Change landing page to Renovation page

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>GibiDroidZ</title>
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="assets/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="assets/css/indexStyle.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class='center-align'>
+      <h4>This page is under renovation.</h4>
+      <br />
+      <i class="fas fa-cog fa-spin"></i>
+      <p id='loadingText'></p>
+    </div>
+
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="assets/js/materialize.js"></script>
+    <script src="assets/js/indexInit.js"></script>
+
+
+
+    <script>
+      var loadingText = [
+        'Picking up apples ..',
+        'Chasing bunnies ..',
+        'Cooking sausages ..',
+        'Getting a haircut ..',
+        'Baking pancakes ..',
+        'Playing mind games ..',
+        'Searching reinders ..',
+        'Googling rainbows ..',
+        'Checking aurora ..',
+        'Shaving beard ..'
+      ]
+
+
+      setInterval(function() {
+        var index = Math.floor((Math.random() * 10));
+        $('#loadingText').text(loadingText[index]);
+        console.log(index);
+      }, 1000);
+    </script>
+
+  </body>`
+
+</html>
